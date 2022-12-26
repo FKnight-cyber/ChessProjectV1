a0b536b30497341d618f75f4aac97bfff2bb1be9
ryannicholas <ryannicholas.vieira@gmail.com>
2022-12-26 16:59:14 -0300

    feat: :sparkles: add piece initial movement

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPosition;
+import chess.ChessPiece;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -12,8 +14,17 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
 
-        sc.close();
+        while(true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
